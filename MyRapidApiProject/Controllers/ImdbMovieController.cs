01b4c2f061f6fa007fc48d7813fff00201842490
Seda Aydemir <seda.aydemir7@gmail.com>
2024-12-19 21:48:54 +0300

    Add project files.

---
+++ MyRapidApiProject/Controllers/ImdbMovieController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRapidApiProject.Models;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace MyRapidApiProject.Controllers
+{
+    public class ImdbMovieController : Controller
+    {
+        public async Task< IActionResult> Index()
+        {
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "67431eb8c3msh123231d4b3e9191p1ffc1fjsn6bbbc174495d" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ImdbMovieViewModel>>(body);
+                return View(values.ToList());
+            }
+            
+        }
+    }
+}
---
+++ MyRapidApiProject/Models/ImdbMovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyRapidApiProject.Models
+{
+    public class ImdbMovieViewModel
+    {
+        public string title { get; set; }
+        public string big_image { get; set; }
+        public string rating { get; set; }
+        public string imdb_link { get; set; }
+    }
+}
